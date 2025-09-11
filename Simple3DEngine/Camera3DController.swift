c63c4c7607f817685c30821bbefe4c456fc9a727
Sergey Kozlov <s.kozlov@blackmana.com>
2025-09-11 04:17:36 +0300

    cameraController modifier

---
+++ Simple3DEngine/Camera3DController.swift
@@ -0,0 +1,79 @@
+//
+//  CameraController.swift
+//  Simple3DEngine
+//
+//  Created by Sergey Kozlov on 11.09.2025.
+//
+
+import SwiftUI
+
+struct Camera3DController: ViewModifier {
+    
+    let camera: Camera3D
+    let onCameraChanged: (() -> Void)?
+    @State private var dragOffset: CGSize = .zero
+    @State private var camXZ = AnimatablePair<Float, Float>(0,0)
+    
+    func body(content: Content) -> some View {
+        VStack {
+            content
+                .gesture(
+                    dragGesture
+               )
+            AnimatableEmitter(value: camXZ) { newValue in
+                camera.position.x = newValue.first
+                camera.position.z = newValue.second
+                print("newValue: \(newValue)")
+                onCameraChanged?()
+            }
+         
+            joystick
+        }
+    }
+    
+    
+    private var joystick: some View {
+        HStack {
+            Button("Forward") {
+                moveCamera(dz: 0.5)
+            }
+            Button("Backward") {
+                moveCamera(dz: -0.5)
+            }
+            Button("Left") {
+                moveCamera(dx: -0.5)
+            }
+            Button("Right") {
+                moveCamera(dx: 0.5)
+            }
+        }
+    }
+    func moveCamera(dx: Float = 0, dz: Float = 0) {
+        camXZ = AnimatablePair(camera.position.x,
+            camera.position.z)
+        withAnimation(.easeInOut(duration: 1)) {
+            camXZ = AnimatablePair(camXZ.first + dx, camXZ.second + dz)
+        }
+    }
+    
+    var dragGesture: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                withAnimation(.easeInOut(duration: 0.1)) {
+                    camera.yaw -= Float(value.translation.width - dragOffset.width) * 0.01
+                    camera.pitch -= Float(value.translation.height - dragOffset.height) * 0.01
+                    dragOffset = value.translation
+                    onCameraChanged?()
+                }
+            }
+            .onEnded { _ in
+                dragOffset = .zero
+            }
+    }
+}
+
+extension View {
+    func camera3DController(_ camera: Camera3D, onCameraChanged: (() -> Void)? = nil) -> some View {
+        self.modifier(Camera3DController(camera: camera, onCameraChanged: onCameraChanged))
+    }
+}
--- Simple3DEngine/Scene3DView.swift
+++ Simple3DEngine/Scene3DView.swift
@@ -12,18 +12,10 @@ struct Scene3DView: View {
     @State private var scene3D: Scene3D = Scene3D.quad
     
     @State private var viewportSize: CGSize?
-    @State private var dragOffset: CGSize = .zero
-    
-    @State private var camXZ = AnimatablePair<Float, Float>(0,0)
+
     
     var body: some View {
         VStack {
-            AnimatableEmitter(value: camXZ) { newValue in
-                scene3D.camera.position.x = newValue.first
-                scene3D.camera.position.z = newValue.second
-                print("newValue: \(newValue)")
-                renderFrame()
-            }
             Group {
                 if let object2D {
                     ShapeFrom3D(object2D: object2D)
@@ -34,63 +26,20 @@ struct Scene3DView: View {
             .onGeometryChange (for: CGSize.self, of: { proxy in proxy.size}) { size in
                 viewportSize = size
                 renderFrame()
-                
             }
-            .gesture(
-                dragGesture
-           )
-            joystick
+
         }
         .padding()
-    }
-    
-    
-    private var joystick: some View {
-        HStack {
-            Button("Forward") {
-                moveCamera(dz: 0.5)
-            }
-            Button("Backward") {
-                moveCamera(dz: -0.5)
-            }
-            Button("Left") {
-                moveCamera(dx: -0.5)
-            }
-            Button("Right") {
-                moveCamera(dx: 0.5)
-            }
-        }
-    }
-    func moveCamera(dx: Float = 0, dz: Float = 0) {
-        camXZ = AnimatablePair(scene3D.camera.position.x,
-            scene3D.camera.position.z)
-        withAnimation(.easeInOut(duration: 1)) {
-            camXZ = AnimatablePair(camXZ.first + dx, camXZ.second + dz)
+        .camera3DController(scene3D.camera) {
+            renderFrame()
         }
     }
     
-    var dragGesture: some Gesture {
-        DragGesture()
-            .onChanged { value in
-                let camera = scene3D.camera
-                withAnimation(.easeInOut(duration: 0.1)) {
-                    camera.yaw -= Float(value.translation.width - dragOffset.width) * 0.01
-                    camera.pitch -= Float(value.translation.height - dragOffset.height) * 0.01
-                    dragOffset = value.translation
-                    renderFrame()
-                }
-            }
-            .onEnded { _ in
-                dragOffset = .zero
-            }
-    }
-    
+
     func renderFrame() {
         guard let viewportSize else  { return }
         let res = engine3D.objects2D(from: scene3D, screenSize: CGSize(width: viewportSize.width, height: viewportSize.width)).first
-        print(res)
         object2D = res
-        print(object2D)
     }
 
 }
