6e2007d8ffdf889b7f30859b9dd79a73e45b0534
Sergey Kozlov <s.kozlov@blackmana.com>
2025-09-12 04:59:44 +0300

    trim

--- Simple3DEngine/Scene3DView.swift
+++ Simple3DEngine/Scene3DView.swift
@@ -13,16 +13,32 @@ struct Scene3DView: View {
     
     @State private var viewportSize: CGSize?
 
+    @State private var trimEnd: CGFloat = 1
+    @State private var isTrimEnabled = false
+    
+    @State private var trimAnimationID = UUID()
     
     var body: some View {
-        ZStack {
+        VStack {
             Group {
                 if let  objects2D, let viewportSize  {
                     ShapeFrom3D(objects2D: objects2D)
+                        .trim(from: 0, to: trimEnd)
                         .stroke(.black)
                         .background(.yellow)
                         .scaleEffect(x: 1, y: -1, anchor: .topLeading)
                         .offset(y: viewportSize.height)
+                        .onAppear() {
+                            if isTrimEnabled {
+                                 withAnimation(.linear(duration: 2).repeatForever(autoreverses: true)) {
+                                    trimEnd = 0
+                                }
+                            }
+                            else {
+                                trimEnd = 1
+                            }
+                        }
+                        .id(isTrimEnabled ? trimAnimationID : UUID())
                     
                 }
             }
@@ -31,6 +47,10 @@ struct Scene3DView: View {
                 scene3D.camera.setAspect(aspect: Float(size.width / size.height))
                 renderFrame()
             }
+            Button("trim effect") {
+                isTrimEnabled.toggle()
+            }
+
         }
         .padding()
 
